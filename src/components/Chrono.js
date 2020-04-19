27c06efa27c26b7efa6aa9b04bd78658cd9648f6
OliveLeb <lebel.olivier@wanadoo.fr>
2020-04-19 18:52:54 +0200

    creation chronometre

--- src/components/Chrono.js
+++ src/components/Chrono.js
@@ -1,54 +1,54 @@
-import React, { useReducer, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-/*
-const reducer = (state, action) => {
-  switch(action.type){
-    case:
-      return
-    case:
-      return
-    default:
-      return state;
-}
-}*/
-
-const Chrono = () => {
-  // const [] = useReducer()
+const Chrono2 = () => {
   const [startTime, setStartTime] = useState(0);
   const [isActive, setIsActive] = useState(false);
-  const [second, setSecond] = useState(0);
+  const [time, setTime] = useState({ h: 0, m: 0, s: 0, ms: 0 });
+  const tiempo = startTime;
 
   const toggleIsActive = () => {
     setIsActive(!isActive);
-    setStartTime(Date.now());
+    setStartTime(Date.now() - tiempo);
   };
+
   const reset = () => {
     setStartTime(0);
     setIsActive(false);
-    setSecond(0);
+    setTime({ h: 0, m: 0, s: 0, ms: 0 });
   };
 
   useEffect(() => {
     let interval = null;
+
     if (isActive) {
       interval = setInterval(() => {
-        setSecond(Math.floor((Date.now() - startTime) / 100));
+        setTime({
+          h: ('0' + Math.floor((Date.now() - tiempo) / 1000 / 3600)).slice(-2),
+          m: ('0' + Math.floor((Date.now() - tiempo) / 1000 / 60)).slice(-2),
+          s: ('0' + Math.floor((Date.now() - tiempo) / 1000)).slice(-2),
+          ms: ('0' + Math.floor((Date.now() - tiempo) / 10)).slice(-2),
+        });
       }, 100);
     } else if (!isActive && startTime !== 0) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [isActive, startTime]);
+  }, [isActive, startTime, time, tiempo]);
 
   return (
     <>
       <div>Le Chrono</div>
 
-      <p>{second}</p>
-      <button onClick={toggleIsActive}>{isActive ? 'Pause' : 'Start'}</button>
+      <p></p>
+      <p>
+        {time.h} : {time.m} : {time.s} : {time.ms}
+      </p>
+      <button onClick={toggleIsActive}>
+        {isActive ? 'Pause' : !isActive && startTime !== 0 ? 'Resume' : 'Start'}
+      </button>
       <button onClick={reset}>Reset</button>
     </>
   );
 };
 
-export default Chrono;
+export default Chrono2;
