a41d30a94701e099e664be56bc6c6a9ceb569893
OliveLeb <lebel.olivier@wanadoo.fr>
2020-04-21 18:07:05 +0200

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
     <>
       <div>Home</div>
       <div>Chronometrez vos perfs, programmez vos séances de sport !</div>
+      <div>2020 - By Olivier Lebel</div>
     </>
   );
 };
